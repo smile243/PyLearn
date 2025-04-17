424bd196a783bd657a40b2594535bb2a1bb93858
yujiale <827359508@qq.com>
2025-04-17 10:15:45 +0800

    util优化

--- util/photo_video/test.py
+++ util/photo_video/test.py
@@ -4,7 +4,8 @@ import json
 
 with open('阿里巴巴站.txt', 'r', encoding='utf-8') as f:
     urls = [line.strip() for line in f.readlines()]
-
+with open('必应.txt', 'r', encoding='utf-8') as f:
+    searchs = [line.strip() for line in f.readlines()]
 browser = Chromium() 
 # 创建页面对象
 page = browser.latest_tab
@@ -13,7 +14,7 @@ download.set.if_file_exists('rename')
 save_path = r'阿里巴巴'
 save_path2 = r'必应'
 imgcount = 1
-videoName = 'maya-die casting-'
+videoName = searchs[0]+'-'
 videocount = 1
 for url in urls:
     page.get(url)
@@ -24,19 +25,18 @@ for url in urls:
             download.add(img_url,save_path,f"{imgcount}")
             imgcount+=1
     video = page.ele('t:video')
-    if video:
+    if video and video.attr('src'):
          download.add(video.attr('src'),save_path,f"{videoName}{videocount}")
          videocount+=1;
 
-with open('必应.txt', 'r', encoding='utf-8') as f:
-    searchs = [line.strip() for line in f.readlines()]
-for search in searchs:    
-    biying = f"https://cn.bing.com/images/search?q={search}&go=Search&qs=ds&form=QBIR&first=1"
-    page.get(biying)
-    hrefs = page.eles('tag:a@@class=iusc')
-    for i,href in enumerate(hrefs, 1):
-        m = href.attr('m')
-        json_m =json.loads(m);
-        img_url = json_m['murl'];
-        download.add(img_url,save_path2,f"{imgcount}")
-        imgcount+=1     
+
+# for search in searchs:    
+#     biying = f"https://cn.bing.com/images/search?q={search}&go=Search&qs=ds&form=QBIR&first=1"
+#     page.get(biying)
+#     hrefs = page.eles('tag:a@@class=iusc')
+#     for i,href in enumerate(hrefs, 1):
+#         m = href.attr('m')
+#         json_m =json.loads(m);
+#         img_url = json_m['murl'];
+#         download.add(img_url,save_path2,f"{imgcount}")
+#         imgcount+=1     
